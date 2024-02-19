2a4d28f4ec2e27299f1924de9a66313a0be3184f
Aryan Panchal <panchalaryan698@gmail.com>
2024-02-19 18:12:48 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Components/Header'
+
+function App() {
+
+  return (
+    <>
+      <div className="header">
+        <Header />
+      </div>
+    </>
+  )
+}
+
+export default App
